744213426d19045d463f8a311759978ad98aef92
Sanket Mishra <sanket14mishra@gmail.com>
2017-05-24 15:54:23 +0530

    Removed console.log statements

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ function populateHTML(jsonURL){
 				items.push(thisJsonItem);
 
 				$('#targetElement').append(generatedHTML);
-				console.log("after"+$(window).scrollTop());
+				// console.log("after"+$(window).scrollTop());
 				$(window).scrollTop(tempScrollTop);
 
 		});
@@ -126,7 +126,7 @@ function populateHTML(jsonURL){
 $('#targetElement').html(" ");
 
 var jsonURL = getWebsiteURL();
-console.log("before"+$(window).scrollTop());
+// console.log("before"+$(window).scrollTop());
 populateHTML(jsonURL);
 
 $(window).on("scroll",function () {
@@ -134,10 +134,10 @@ $(window).on("scroll",function () {
 		$(".load").removeClass("load");
 		jsonURL = getWebsiteURL();
 		tempScrollTop=$(window).scrollTop();
-		console.log("before"+$(window).scrollTop());
+		// console.log("before"+$(window).scrollTop());
 		populateHTML(jsonURL);
 		$(window).scrollTop(tempScrollTop);
-		console.log("Need  to load");
+		// console.log("Need  to load");
 	}
 });
 
